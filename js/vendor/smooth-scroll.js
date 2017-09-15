e0abac414b5f3b22212972e6bd790dba018958b8
laurenhallden <lhallden@rjmetrics.com>
2017-09-15 09:24:16 -0400

    simplifying smooth scrool funtions

--- js/vendor/smooth-scroll.js
+++ js/vendor/smooth-scroll.js
@@ -1,34 +1,3 @@
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a.button").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-      var heading = $(hash).find('h2');
-
-      var currentSection = $("body").find('#' + start);
-      $(currentSection).nextAll().addClass('hidden');
-      $(hash).removeClass('hidden');
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(heading).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
-
-
 function scrollTo(end){
     hash = end;
 
